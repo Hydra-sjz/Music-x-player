dfdabb2c5915d05d662acb3884f50d7771099f9e
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-27 22:50:32 +0530

    Update and rename bot.py to bot.py

--- PROFESSOR/core/bot.py
+++ PROFESSOR/core/bot.py
@@ -6,11 +6,11 @@ import config
 from ..logging import LOGGER
 
 
-class DAXX(Client):
+class PROF(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="DAXXMUSIC",
+            name="PROFESSOR",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
