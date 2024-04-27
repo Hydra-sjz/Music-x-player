6e2465de6bcd7f8c82c843a59e938198df65dd16
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-27 14:52:40 +0530

    Update reload.py

--- DAXXMUSIC/plugins/tools/reload.py
+++ DAXXMUSIC/plugins/tools/reload.py
@@ -103,7 +103,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(7171272363)
+    & filters.user(7074689169)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
