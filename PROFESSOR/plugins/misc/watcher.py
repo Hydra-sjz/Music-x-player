2e3359838d28db8265dc41423840fa40a96c6d38
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 13:48:30 +0530

    Update and rename watcher.py to watcher.py

--- PROFESSOR/plugins/misc/watcher.py
+++ PROFESSOR/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.core.call import DAXX
+from PROFESSOR import app
+from PROFESSOR.core.call import PROF
 
 welcome = 20
 close = 30
@@ -11,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await DAXX.stop_stream_force(message.chat.id)
+    await PROF.stop_stream_force(message.chat.id)
