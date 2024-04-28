c6cacfbafed15371dc75807ab64fd7080ce3a224
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 14:16:14 +0530

    Update and rename blchat.py to blchat.py

--- PROFESSOR/plugins/sudo/blchat.py
+++ PROFESSOR/plugins/sudo/blchat.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
-from DAXXMUSIC.utils.decorators.language import language
+from PROFESSOR import app
+from PROFESSOR.misc import SUDOERS
+from PROFESSOR.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
+from PROFESSOR.utils.decorators.language import language
 from config import BANNED_USERS
 
 
