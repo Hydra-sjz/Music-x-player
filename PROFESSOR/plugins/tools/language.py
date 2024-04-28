834ed029341074f3cb0ae7d61bd521116fff766f
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 17:27:34 +0530

    Update and rename language.py to language.py

--- PROFESSOR/plugins/tools/language.py
+++ PROFESSOR/plugins/tools/language.py
@@ -2,9 +2,9 @@ from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.database import get_lang, set_lang
-from DAXXMUSIC.utils.decorators import ActualAdminCB, language, languageCB
+from PROFESSOR import app
+from PROFESSOR.utils.database import get_lang, set_lang
+from PROFESSOR.utils.decorators import ActualAdminCB, language, languageCB
 from config import BANNED_USERS
 from strings import get_string, languages_present
 
