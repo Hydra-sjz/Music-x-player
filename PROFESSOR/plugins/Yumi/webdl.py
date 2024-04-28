08f2d78105a5623f62f76d3594650eaff339fa32
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 10:46:03 +0530

    Update and rename webdl.py to webdl.py

--- PROFESSOR/plugins/Yumi/webdl.py
+++ PROFESSOR/plugins/Yumi/webdl.py
@@ -3,7 +3,7 @@ import requests
 from pyrogram import Client, filters
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
-from DAXXMUSIC import app
+from PROFESSOR import app
 
 
 def download_website(url):
