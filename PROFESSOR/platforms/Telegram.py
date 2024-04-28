f461902a322758166b1d2f420e7805adbb4c1eaf
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 07:57:08 +0530

    Update and rename Telegram.py to Telegram.py

--- PROFESSOR/platforms/Telegram.py
+++ PROFESSOR/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.formatters import (
+from PROFESSOR import app
+from PROFESSOR.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
