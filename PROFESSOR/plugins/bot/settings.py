e7d3ca8e3e39bfb427d0b97be62c56a3d17394c9
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 12:28:52 +0530

    Update and rename settings.py to settings.py

--- PROFESSOR/plugins/bot/settings.py
+++ PROFESSOR/plugins/bot/settings.py
@@ -8,8 +8,8 @@ from pyrogram.types import (
     Message,
 )
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.database import (
+from PROFESSOR import app
+from PROFESSOR.utils.database import (
     add_nonadmin_chat,
     get_authuser,
     get_authuser_names,
@@ -25,15 +25,15 @@ from DAXXMUSIC.utils.database import (
     skip_off,
     skip_on,
 )
-from DAXXMUSIC.utils.decorators.admins import ActualAdminCB
-from DAXXMUSIC.utils.decorators.language import language, languageCB
-from DAXXMUSIC.utils.inline.settings import (
+from PROFESSOR.utils.decorators.admins import ActualAdminCB
+from PROFESSOR.utils.decorators.language import language, languageCB
+from PROFESSOR.utils.inline.settings import (
     auth_users_markup,
     playmode_users_markup,
     setting_markup,
     vote_mode_markup,
 )
-from DAXXMUSIC.utils.inline.start import private_panel
+from PROFESSOR.utils.inline.start import private_panel
 from config import BANNED_USERS, OWNER_ID
 
 
