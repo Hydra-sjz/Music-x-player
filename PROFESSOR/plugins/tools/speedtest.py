7057b2863d94e8fd418696f4bd543949001625b1
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 21:46:09 +0530

    Update and rename speedtest.py to speedtest.py

--- PROFESSOR/plugins/tools/speedtest.py
+++ PROFESSOR/plugins/tools/speedtest.py
@@ -4,9 +4,9 @@ import speedtest
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.decorators.language import language
+from PROFESSOR import app
+from PROFESSOR.misc import SUDOERS
+from PROFESSOR.utils.decorators.language import language
 
 
 def testspeed(m, _):
