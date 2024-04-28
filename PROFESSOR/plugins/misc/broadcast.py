d45fd2313816f931e06e4313d86818a1f4d92ac6
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 13:15:17 +0530

    Update and rename broadcast.py to broadcast.py

--- PROFESSOR/plugins/misc/broadcast.py
+++ PROFESSOR/plugins/misc/broadcast.py
@@ -4,17 +4,17 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import (
+from PROFESSOR import app
+from PROFESSOR.misc import SUDOERS
+from PROFESSOR.utils.database import (
     get_active_chats,
     get_authuser_names,
     get_client,
     get_served_chats,
     get_served_users,
 )
-from DAXXMUSIC.utils.decorators.language import language
-from DAXXMUSIC.utils.formatters import alpha_to_int
+from PROFESSOR.utils.decorators.language import language
+from PROFESSOR.utils.formatters import alpha_to_int
 from config import adminlist
 
 IS_BROADCASTING = False
