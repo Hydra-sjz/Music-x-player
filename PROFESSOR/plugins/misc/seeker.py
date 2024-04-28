affcec3932da600bfa0c11b83e10a744af89fe95
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 13:35:18 +0530

    Update and rename seeker.py to seeker.py

--- PROFESSOR/plugins/misc/seeker.py
+++ PROFESSOR/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from DAXXMUSIC.misc import db
-from DAXXMUSIC.utils.database import get_active_chats, is_music_playing
+from PROFESSOR.misc import db
+from PROFESSOR.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
