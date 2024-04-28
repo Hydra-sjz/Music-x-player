c4cd611d306c970be1865e635186935c6c0ac2da
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 08:46:07 +0530

    Update and rename fakeinfo.py to fakeinfo.py

--- PROFESSOR/plugins/Yumi/fakeinfo.py
+++ PROFESSOR/plugins/Yumi/fakeinfo.py
@@ -1,7 +1,7 @@
 """***
 MIT License
 
-Copyright (c) [2023] [DAXX TEAM]
+Copyright (c) [2024] [ SOURABH PROFESSOR ]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -25,7 +25,7 @@ SOFTWARE.***
 import requests
 from pyrogram import Client
 from pyrogram import filters
-from DAXXMUSIC import app
+from PROFESSOR import app
 
 
 random_user_api_url = 'https://randomuser.me/api/'
