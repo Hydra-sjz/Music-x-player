15d086eaf7b10ac5a3f55c97785793783baf4584
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 14:25:02 +0530

    Update and rename maintenance.py to maintenance.py

--- PROFESSOR/plugins/sudo/maintenance.py
+++ PROFESSOR/plugins/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import (
+from PROFESSOR import app
+from PROFESSOR.misc import SUDOERS
+from PROFESSOR.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
