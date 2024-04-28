c768aa90ec4093cb06c3835569f622650f512882
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 12:20:59 +0530

    Update and rename help.py to help.py

--- PROFESSOR/plugins/bot/help.py
+++ PROFESSOR/plugins/bot/help.py
@@ -3,11 +3,11 @@ from typing import Union
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils import help_pannel
-from DAXXMUSIC.utils.database import get_lang
-from DAXXMUSIC.utils.decorators.language import LanguageStart, languageCB
-from DAXXMUSIC.utils.inline.help import help_back_markup, private_help_panel
+from PROFESSOR import app
+from PROFESSOR.utils import help_pannel
+from PROFESSOR.utils.database import get_lang
+from PROFESSOR.utils.decorators.language import LanguageStart, languageCB
+from PROFESSOR.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
 
