db2e019c3665027ea820b858f7d95bbddcc8747a
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 10:06:48 +0530

    Update and rename kangstickers.py to kangstickers.py

--- PROFESSOR/plugins/Yumi/kangstickers.py
+++ PROFESSOR/plugins/Yumi/kangstickers.py
@@ -14,17 +14,17 @@ from pyrogram.errors import (
     UserIsBlocked,
 )
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from DAXXMUSIC import app
+from PROFESSOR import app
 from config import BOT_USERNAME
-from DAXXMUSIC.utils.errors import capture_err
+from PROFESSOR.utils.errors import capture_err
 
-from DAXXMUSIC.utils.files import (
+from PROFESSOR.utils.files import (
     get_document_from_file_id,
     resize_file_to_sticker_size,
     upload_document,
 )
 
-from DAXXMUSIC.utils.stickerset import (
+from PROFESSOR.utils.stickerset import (
     add_sticker_to_set,
     create_sticker,
     create_sticker_set,
