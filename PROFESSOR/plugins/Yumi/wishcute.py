74d405f369738ef77471061894b9bc0b4d29ab4a
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 10:42:18 +0530

    Update and rename wishcute.py to wishcute.py

--- PROFESSOR/plugins/Yumi/wishcute.py
+++ PROFESSOR/plugins/Yumi/wishcute.py
@@ -2,9 +2,9 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import random
 import requests
-from DAXXMUSIC import app 
+from PROFESSOR import app 
 
-SUPPORT_CHAT = "The_friendz"
+SUPPORT_CHAT = "Friends_Chatting_Group_Friends_0"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
@@ -19,7 +19,7 @@ async def wish(_, m):
     wish = f"๏ ʜᴇʏ {m.from_user.first_name}!"
     wish += f"\n๏ ʏᴏᴜʀ ᴡɪꜱʜ ➠ {text} "
     wish += f"\n๏ ᴘᴏꜱꜱɪʙʟᴇ ᴛᴏ ➠ {wish_count}%"
-    wish += f"\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐"
+    wish += f"\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ᴘʀᴏғᴇssᴏʀ "
     
     await app.send_animation(
         chat_id=m.chat.id,
