de8c89a3cde883cb578d3992e0339121ef40bb4f
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 12:04:59 +0530

    Update and rename stop.py to stop.py

--- PROFESSOR/plugins/admins/stop.py
+++ PROFESSOR/plugins/admins/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.core.call import DAXX
-from DAXXMUSIC.utils.database import set_loop
-from DAXXMUSIC.utils.decorators import AdminRightsCheck
-from DAXXMUSIC.utils.inline import close_markup
+from PROFESSOR import app
+from PROFESSOR.core.call import PROF
+from PROFESSOR.utils.database import set_loop
+from PROFESSOR.utils.decorators import AdminRightsCheck
+from PROFESSOR.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -16,7 +16,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await DAXX.stop_stream(chat_id)
+    await PROF.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
