ab081f24696655860b341f1fbcfbf1cfccc6af94
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 13:39:27 +0530

    Update and rename state.py to state.py

--- PROFESSOR/plugins/misc/state.py
+++ PROFESSOR/plugins/misc/state.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import pycountry
-from DAXXMUSIC import app 
+from PROFESSOR import app 
 
 @app.on_message(filters.command("gst"))
 def get_states(client, message):
