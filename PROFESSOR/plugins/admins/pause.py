148c9964724355f1280e5bdcd45814366876ae07
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 11:15:33 +0530

    Update and rename pause.py to pause.py

--- PROFESSOR/plugins/admins/pause.py
+++ PROFESSOR/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.core.call import DAXX
-from DAXXMUSIC.utils.database import is_music_playing, music_off
-from DAXXMUSIC.utils.decorators import AdminRightsCheck
-from DAXXMUSIC.utils.inline import close_markup
+from PROFESSOR import app
+from PROFESSOR.core.call import PROF
+from PROFESSOR.utils.database import is_music_playing, music_off
+from PROFESSOR.utils.decorators import AdminRightsCheck
+from PROFESSOR.utils.inline import close_markup
 from config import BANNED_USERS
 
 
