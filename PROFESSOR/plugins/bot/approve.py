ae002652ffe8c2b6ffcbb1821cc8d832dc38c511
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 12:19:11 +0530

    Update and rename approve.py to approve.py

--- PROFESSOR/plugins/bot/approve.py
+++ PROFESSOR/plugins/bot/approve.py
@@ -1,4 +1,4 @@
-from DAXXMUSIC import app
+from PROFESSOR import app
 from os import environ
 from pyrogram import Client, filters
 from pyrogram.types import ChatJoinRequest
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/nykaaxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Professor_Sukoon_Bot?startgroup=true"),
     ],
 ]
 
