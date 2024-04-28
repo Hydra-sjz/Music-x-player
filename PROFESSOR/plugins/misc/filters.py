748ee9421611e712eafe235273cc1ed8e6a951a9
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-28 13:17:12 +0530

    Update and rename filters.py to filters.py

--- PROFESSOR/plugins/misc/filters.py
+++ PROFESSOR/plugins/misc/filters.py
@@ -1,10 +1,10 @@
 import re
-from DAXXMUSIC import app
+from PROFESSOR import app
 from config import BOT_USERNAME
-from DAXXMUSIC.utils.daxx_ban import admin_filter
-from DAXXMUSIC.mongo.filtersdb import *
-from DAXXMUSIC.utils.filters_func import GetFIlterMessage, get_text_reason, SendFilterMessage
-from DAXXMUSIC.utils.yumidb import user_admin
+from PROFESSOR.utils.prof_ban import admin_filter
+from PROFESSOR.mongo.filtersdb import *
+from PROFESSOR.utils.filters_func import GetFIlterMessage, get_text_reason, SendFilterMessage
+from PROFESSOR.utils.yumidb import user_admin
 from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
