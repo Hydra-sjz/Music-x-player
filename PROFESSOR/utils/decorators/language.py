54334458ad4058b6bc48d3f8e9d8793ec8e50195
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-29 09:05:18 +0530

    Update and rename language.py to language.py

--- PROFESSOR/utils/decorators/language.py
+++ PROFESSOR/utils/decorators/language.py
@@ -1,5 +1,5 @@
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import get_lang, is_maintenance
+from PROFESSOR.misc import SUDOERS
+from PROFESSOR.utils.database import get_lang, is_maintenance
 from strings import get_string
 
 
