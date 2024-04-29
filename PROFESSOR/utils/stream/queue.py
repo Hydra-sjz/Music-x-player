493722da2cf2f5a9311598c31da37167a4eb1847
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-29 08:25:47 +0530

    Update and rename queue.py to queue.py

--- PROFESSOR/utils/stream/queue.py
+++ PROFESSOR/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from DAXXMUSIC.misc import db
-from DAXXMUSIC.utils.formatters import check_duration, seconds_to_min
+from PROFESSOR.misc import db
+from PROFESSOR.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
