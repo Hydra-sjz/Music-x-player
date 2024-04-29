ab93744488029ff7360a6918a1191fee30f29dbd
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-29 08:44:45 +0530

    Update and rename queue.py to queue.py

--- PROFESSOR/utils/inline/queue.py
+++ PROFESSOR/utils/inline/queue.py
@@ -1,6 +1,6 @@
 from typing import Union
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.formatters import time_to_seconds
+from PROFESSOR import app
+from PROFESSOR.utils.formatters import time_to_seconds
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
