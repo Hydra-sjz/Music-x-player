7bfb9a0ecb77251b73ec17a2fd754436fd41efd2
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-04-29 08:21:42 +0530

    Update and rename stream.py to stream.py

--- PROFESSOR/utils/stream/stream.py
+++ PROFESSOR/utils/stream/stream.py
@@ -5,15 +5,15 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from DAXXMUSIC import Carbon, YouTube, app
-from DAXXMUSIC.core.call import DAXX
-from DAXXMUSIC.misc import db
-from DAXXMUSIC.utils.database import add_active_video_chat, is_active_chat
-from DAXXMUSIC.utils.exceptions import AssistantErr
-from DAXXMUSIC.utils.inline import aq_markup, close_markup, stream_markup
-from DAXXMUSIC.utils.pastebin import DAXXBin
-from DAXXMUSIC.utils.stream.queue import put_queue, put_queue_index
-from DAXXMUSIC.utils.thumbnails import get_thumb
+from PROFESSOR import Carbon, YouTube, app
+from PROFESSOR.core.call import PROF
+from PROFESSOR.misc import db
+from PROFESSOR.utils.database import add_active_video_chat, is_active_chat
+from PROFESSOR.utils.exceptions import AssistantErr
+from PROFESSOR.utils.inline import aq_markup, close_markup, stream_markup
+from PROFESSOR.utils.pastebin import PROFBin
+from PROFESSOR.utils.stream.queue import put_queue, put_queue_index
+from PROFESSOR.utils.thumbnails import get_thumb
 
 
 async def stream(
@@ -32,7 +32,7 @@ async def stream(
     if not result:
         return
     if forceplay:
-        await DAXX.force_stop_stream(chat_id)
+        await PROF.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
