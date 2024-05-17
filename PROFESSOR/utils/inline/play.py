66382afe9b3c8d61ee9baafaa7c94c1510ba9865
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-05-17 20:10:23 +0530

    Update play.py

--- PROFESSOR/utils/inline/play.py
+++ PROFESSOR/utils/inline/play.py
@@ -60,25 +60,34 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+                 [
             InlineKeyboardButton(
 
-                text="⦿ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ⦿",
+                text="ᴀᴅᴅ ᴍᴇ",
 
-                url=f"https://t.me/{app.username}?startgroup=true",
+                url=f"https://t.me/{app.username}?startgroup=true",",
+
+            ),
         ],
         [
             InlineKeyboardButton(
 
-                text="⦿ ᴏᴡɴᴇʀ ⦿",
+                text="ᴏᴡɴᴇʀ",
 
                 url=f"t.me/{OWNER_USERNAME}",
 
             ),
             InlineKeyboardButton(
 
-                text="⦿ sᴜᴘᴘᴏʀᴛ ⦿",
+                text="sᴜᴘᴘᴏʀᴛ",
 
                 url=f"{SUPPORT_CHAT}",
+
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -88,30 +97,32 @@ def stream_markup_timer(_, chat_id, played, dur):
 
 def stream_markup(_, chat_id):
     buttons = [
-                [
+        [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+                 [
             InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
+
+                text="ᴀᴅᴅ ᴍᴇ",
+
+                url=f"https://t.me/{app.username}?startgroup=true",",
+
+            ),
         ],
         [
             InlineKeyboardButton(
 
-                text="⦿ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ⦿",
-
-                url=f"https://t.me/{app.username}?startgroup=true",
-        ],
-       [
-            InlineKeyboardButton(
-
-                text="⦿ ᴏᴡɴᴇʀ ⦿",
+                text="ᴏᴡɴᴇʀ",
 
                 url=f"t.me/{OWNER_USERNAME}",
 
             ),
             InlineKeyboardButton(
 
-                text="⦿ sᴜᴘᴘᴏʀᴛ ⦿",
+                text="sᴜᴘᴘᴏʀᴛ",
 
                 url=f"{SUPPORT_CHAT}",
             ),
