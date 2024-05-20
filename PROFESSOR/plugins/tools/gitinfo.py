dd8a770e4e8575692884d8e786430b7a202fe532
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-05-20 19:49:10 +0530

    Update gitinfo.py

--- PROFESSOR/plugins/tools/gitinfo.py
+++ PROFESSOR/plugins/tools/gitinfo.py
@@ -19,7 +19,7 @@ async def blackpink(_, message):
 @app.on_message(filters.command(["github", "git"]))
 async def github(_, message):
     if len(message.command) != 2:
-        await message.reply_text("/git 𝐏𝐑𝚯𝐅𝐄𝐒𝐒𝚯𝐑)
+        await message.reply_text("/git PROFESSOR TERA BAAP HAI BHOSDIKE)
         return
 
     username = message.text.split(None, 1)[1]
