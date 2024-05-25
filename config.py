43fc58fa3ca74fde19c5bb69ebe2caf0e185de3d
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-05-25 08:25:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -51,8 +51,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/xbots_x")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/songdownload_group")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
