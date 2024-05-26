5a7a3c74063e861c8cc9679849745d29ed005fab
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-05-26 15:19:52 +0530

    Update seek.py

--- PROFESSOR/plugins/admins/seek.py
+++ PROFESSOR/plugins/admins/seek.py
@@ -70,6 +70,7 @@ async def seek_comm(cli, message: Message, _, chat_id):
     else:
         db[chat_id][0]["played"] += duration_to_skip
     await mystic.edit_text(
-        text=_["admin_25"].format(seconds_to_min(to_seek), message.from_user.mention),
+        text=_["admin_25"].format(seconds_to_min(to_seek)),
         reply_markup=close_markup(_),
     )
+   #message.from_user.mention
